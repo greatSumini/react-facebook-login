b1a73abe0dfb599c0f07fd3aef6722b88a0e43c3
greatSumini <greatsumini@gmail.com>
2021-05-02 07:58:12 +0900

    style: :bulb: apply jsdoc @default

--- src/types/params.ts
+++ src/types/params.ts
@@ -11,30 +11,22 @@ export type InitParams = {
    * Determines which versions of the Graph API and any API dialogs or plugins are invoked
    *
    * (available versions: https://developers.facebook.com/docs/graph-api/changelog)
-   *
-   * Default value is '9.0'
-   */
+   * @default '9.0' */
   version?: string;
 
   /**
    * Determines whether a cookie is created for the session or not. If enabled, it can be accessed by server-side code.
-   *
-   * Default value is false
-   */
+   * @default false */
   cookie?: boolean;
 
   /**
    * Determines whether a long-lived access token for the session can be saved in localStorage. This enables maintaining a user's logged in status when 3rd party cookies are blocked from being sent to Facebook domains.
-   *
-   * Default value is true
-   */
+   * @default true */
   localStorage?: boolean;
 
   /**
    * Determines whether XFBML tags used by social plugins are parsed, and therefore whether the plugins are rendered or not.
-   *
-   * Default value is false
-   */
+   * @default false */
   xfbml?: boolean;
 };
 
@@ -46,59 +38,46 @@ export type DialogParams = {
 
   /**
    * The URL that you want to redirect the person logging in back to. This URL will capture the response from the Login Dialog. If you are using this in a webview within a desktop app, this must be set to https://www.facebook.com/connect/login_success.html. You can confirm that this URL is set for your app in the App Dashboard. Under Products in the App Dashboard's left side navigation menu, click Facebook Login, then click Settings. Verify the Valid OAuth redirect URIs in the Client OAuth Settings section.
-   *
-   * Default value is (window?.location.href || '/')
-   */
+   * @default (window?.location.href || '/') */
   redirect_uri: string;
 
   /**
    * A string value created by your app to maintain state between the request and callback. This parameter should be used for preventing Cross-site Request Forgery and will be passed back to you, unchanged, in your redirect URI.
-   *
-   * Default value is 'facebookdirect'
-   */
+   * @default 'facebookdirect' */
   state: string;
 
   /**
    * Determines whether the response data included when the redirect back to the app occurs is in URL parameters or fragments.
    *
    * This can be one of ('code', 'token', 'code%20token', 'grated_scopes')
-   *
-   * Default value is 'code'
-   */
+   * @default 'code' */
   response_type?: string;
 
   /**
    * Comma seperated list of permissions to request during login.
    *
    * (available permissions: https://developers.facebook.com/docs/permissions/reference)
-   *
-   * Default value is 'public_profile, email'
-   */
+   * @default 'public_profile, email' */
   scope?: string;
 };
 
 export type LoginOptions = {
   /**
    * supports 3 values: rerequest, reauthenticate, reauthorize.
-   *
-   * Default value is ''
-   */
+   * @default '' */
   auth_type?: string;
 
   scope: string;
 
   /**
    * When true, the granted scopes will be returned in a comma-separated list in the grantedScopes field of the authResponse
-   *
-   * Default value is false
+   * @default false
    */
   return_scopes?: boolean;
 };
 
 export type Props = Pick<InitParams, 'appId'> & {
-  /**
-   * Default value is 'en_US'
-   */
+  /** @default 'en_US' */
   language?: string;
 
   /**
@@ -112,9 +91,7 @@ export type Props = Pick<InitParams, 'appId'> & {
 
   /**
    * fields return by /me
-   *
-   * Default value is 'name,email,picture'
-   */
+   * @default 'name,email,picture' */
   fields?: string;
 
   onSuccess: (res: LoginResponse['authResponse']) => void;
@@ -123,7 +100,8 @@ export type Props = Pick<InitParams, 'appId'> & {
 
   onProfileSuccess?: (res: unknown) => void;
 
-  /** Children Component @default "Login with Facebook" */
+  /** Children Component
+   * @default "Login with Facebook" */
   children?: ReactChild;
 
   /**
@@ -140,9 +118,7 @@ export type Props = Pick<InitParams, 'appId'> & {
 
   /**
    * if true, use window.FB.login in mobile agent
-   *
-   * Default value is false
-   */
+   * @default false */
   disableMobileRedirect?: boolean;
 
   initParams?: Omit<InitParams, 'appId'>;
