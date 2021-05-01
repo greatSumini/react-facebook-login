d550c9b55f47dd9442a9f7bf2f5e6b7298b1dbc1
greatSumini <greatsumini@gmail.com>
2021-05-02 06:46:56 +0900

    chore: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    ecmaVersion: 2020,
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
 
