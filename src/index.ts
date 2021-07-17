c27c7a1f3a96b80896c67abd284602617a54e538
greatSumini <greatSumini@gmail.com>
2021-07-17 21:55:07 +0900

    feat: export helpers, FacebookClient

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import FacebookLogin from './facebook-login';
 
+export * from './helpers';
 export * from './types';
 export * from './facebook-login';
+export * from './facebook.client';
 
 export default FacebookLogin;
